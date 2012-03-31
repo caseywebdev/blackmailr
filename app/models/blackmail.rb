8c9e9bd422a80cc463ed6b610a5d3e9769c5c377
Katie Soto <KReneSoto@gmail.com>
2012-03-30 17:33:30 -0700

    Updated blackmails schema

--- app/models/blackmail.rb
+++ app/models/blackmail.rb
@@ -3,14 +3,14 @@
 # Table name: blackmails
 #
 #  id         	:integer    	not null, primary key
-#  title       	:string(255)
-#  description       :string(255)
-#  victim_name       :string(255)
-#  victim_email      :string(255)	not null
+#  title       	:string(255)	not null
+#  description  :string(255)	not null
+#  victim_name  :string(255)	not null
+#  victim_email :string(255)	not null
 #  expired_at 	:datetime
-#  created_at 	:datetime
-#  updated_at 	:datetime
-#  :user is referenced		not null
+#  created_at 	:datetime		not null
+#  updated_at 	:datetime		not null
+#  user_id references user		not null
 #
 
 class Blackmail < ActiveRecord::Base
