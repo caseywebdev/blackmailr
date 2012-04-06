a175b1de47b2ddd7f3d21b12e84157fe40587527
Katie Soto <KReneSoto@gmail.com>
2012-04-06 16:15:38 -0700

    Added /sign-up form, although not fully functional

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,21 @@ class UsersController < ApplicationController
   end
   
   def new
+    @user = User.new
   end
   
   def create
+    #create a new user with all data received from form (in params)
+    @user = User.new(params[:user])
+    #once @user is defined properly, calling @user.save is all that’s needed to complete the registration
+    if @user.save
+        #Handle a successful save.
+        flash[:success] = "Registration Successful!"
+        redirect_to root_path
+    else
+    #re-render the signup page if invalid signup data is received.
+      render 'new'
+    end
   end
   
   def edit
