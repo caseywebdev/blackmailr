02aa35f99f649520f1f8a324824c84a044110a09
Katie Soto <KReneSoto@gmail.com>
2012-04-11 16:45:30 -0700

    Added some logic to the 'sign_in' action, and added TODOs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     #once @user is defined properly, calling @user.save is all that’s needed to complete the registration
     if @user.save
         #Handle a successful save.
+        #sign_in @user #after a new user signs up, log them in
         flash[:success] = "Registration Successful!"
         redirect_to root_path
     else
@@ -22,7 +23,9 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-  
+
+#TODO  
+#users "profile"
   def edit
   end
   
@@ -36,12 +39,22 @@ class UsersController < ApplicationController
   def sign_in_form
      @user = User.new
   end
-  
+
+#TODO (currently the sign in form is trying to insert?)
 #post request goes to this action (from the form) to sign the user in  
   def sign_in
      #use the params given by sign_in_form to actually sign the user in
+    User.find_by_email(params[:session][:email]).try(:authenticate, params[:session][:password])
+    if user.nil?
+      flash.now[:error] = "Invalid email/password combination."
+      render 'sign_in_form'
+    else
+      #sign_in user
+      redirect_to user
+    end
   end
   
+#TODO  
 #sign the user out (destroy the session/cookies)  
   def sign_out
   end
